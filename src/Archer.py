ea1a9ec188375cc8fe256eb9db92f650d5168fc5
calandula <36953570+calandula@users.noreply.github.com>
2023-03-13 17:52:18 +0100

    base class of Piece and subclass

--- src/Archer.py
+++ src/Archer.py
@@ -1,4 +1,54 @@
 import Piece
 
 class Archer(Piece):
-    ...
+    def __init__(self, color) -> None:
+        super().__init__("archer", color)
+
+    def move(self, start, end):
+        if self.is_orthogonal(start, end):
+            print(f"{self.color} {self.type} moves to {end}.")
+            return True
+        else:
+            print("Invalid move, archers can only move one space orthogonally.")
+            return False
+        
+    def attack(self, start, end):
+        if self.separated_by_two_spaces(start, end):
+            print(f"{self.color} {self.type} attacks {end}!")
+            return True
+        else:
+            print("Invalid attack, archers can only attack from 2 spaces.")
+            return False
+            
+    
+    def is_adjacent(self, start, end):
+        x1, y1 = self.get_coordinates(start)
+        x2, y2 = self.get_coordinates(end)
+        if abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1:
+            return True
+        else:
+            return False
+        
+    def separated_by_two_spaces(self, start, end):
+        x1, y1 = self.get_coordinates(start)
+        x2, y2 = self.get_coordinates(end)
+        if abs(x1 - x2) + abs(y1 - y2) == 2:
+            return True
+        else:
+            return False
+
+    def is_orthogonal(self, start, end):
+        x1, y1 = self.get_coordinates(start)
+        x2, y2 = self.get_coordinates(end)
+        if x1 == x2 or y1 == y2:
+            return True
+        else:
+            return False
+
+    def get_coordinates(self, position):
+        column, row = position
+        x = ord(column) - 97
+        y = int(row) - 1
+        return x, y
+
+    
--- src/Piece.py
+++ src/Piece.py
@@ -1,2 +1,32 @@
 class Piece:
-    ...
+    def __init__(self, type, color) -> None:
+        self.type = type
+        self.color = color
+        self.captured = False
+        
+    def get_move(self):
+        while True:
+            start = input("Enter the starting position of the piece (ex. a8): ")
+            end = input("Enter the ending position of the piece (ex. b8): ")
+            if not self.is_valid_position(start) or not self.is_valid_position(end):
+                    print("Invalid position, please enter a valid position.")
+                    continue
+            return start, end
+        
+    def move(self, start, end):
+        pass
+
+    def attack(self, start, end):
+        pass
+
+    def is_valid_position(self, position):
+        columns = "abcde"
+        rows = "12345"
+        if len(position) != 2:
+            return False
+        column, row = position
+        if column not in columns or row not in rows:
+            return False
+        return True
+    
+    
