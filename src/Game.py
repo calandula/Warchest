7ea5d35f718aa0bbfe782c8ebef78b99a28ba8db
calandula <36953570+calandula@users.noreply.github.com>
2023-03-14 18:05:38 +0100

    Game and Player blueprints

--- src/Game.py
+++ src/Game.py
@@ -1,2 +1,22 @@
+import Player
+import random
+import Board
+
 class Game:
-    ...
+    def __init__(self) -> None:
+        self.players = [Player("Wolf"), Player("Crow")]
+        self.bag = {
+            "archer": 4,
+            "berserker": 4,
+            "cavalry": 4,
+            "crossbowman": 5,
+        }
+        self.types = [key for key in self.bag.keys()]
+        self.game_over = False
+        self.turn_player_index = 0
+        self.players[0].assign_types(random.shuffle(self.types)[0:2], self.bag)
+        self.players[1].assign_types(random.shuffle(self.types)[2:4], self.bag)
+
+
+
+    
--- src/Player.py
+++ src/Player.py
@@ -1,2 +1,46 @@
+import random
+
 class Player:
-    ...
+    def __init__(self, clan) -> None:
+        self.clan = clan
+        self.bag = []
+        self.hand = []
+        self.discard_pile = []
+        self.recruitment = []
+        self.control_token = 4
+
+    def assign_types(self, types, typeCount):
+        self.bag.append("royal")
+        for type in types:
+            self.bag.append(type)
+            self.bag.append(type)
+            typeCount[type] -= 2
+
+        self.recruitment = {key:value for (key, value) in typeCount if key in types}
+
+    def draw_units(self):
+        if self.bag < 3:
+            self.bag = [*self.bag, *self.discard_pile]
+            self.discard_pile = []
+        self.hand = random.shuffle(self.bag)[0:3]
+
+    def discard_unit(self):
+        ...
+    
+    def place_unit(self):
+        ...
+
+    def control_zone(self):
+        ...
+
+    def move_unit(self):
+        ...
+
+    def recruit_unit(self):
+        ...
+
+    def attack_unit(self):
+        ...
+
+    def take_initiative(self):
+        ...
